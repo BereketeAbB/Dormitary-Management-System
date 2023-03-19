8ea82056cf33ebb7019cdbf2805f54ba18a7da0a
BereketeAbB <79558330+BereketeAbB@users.noreply.github.com>
2023-03-19 22:41:02 +0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,15 @@
+package com.mycompany.dms;
+
+/**
+ *  Person superclass
+ *
+ * @author bereketeab
+ */
+public class Person {
+
+  String name;  
+    Person(String name){
+        this.name = name;
+    }
+  
+}
