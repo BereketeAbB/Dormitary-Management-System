8b8293d6817cffcedadee5328b5de2f1458f482c
BereketeAbB <79558330+BereketeAbB@users.noreply.github.com>
2023-03-19 22:39:55 +0300

    Create DMS.java

---
+++ DMS.java
@@ -0,0 +1,16 @@
+package com.mycompany.dms;
+
+
+/**
+ * class with main function 
+ *
+ * @author bereketeab
+ */
+public class DMS {
+ 
+    public static void main(String[] args) {
+        UI ui = new UI();
+        ui.login();
+    }
+  
+}
